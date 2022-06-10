865216e6be9c8f1e268f26b975e677bf0c2b6d6c
Miina Lervik <miina.lervik@bekk.no>
2022-06-10 15:08:28 +0200

    skilt ut fasit

--- src/6_generics/fasit.ts
+++ src/6_generics/fasit.ts
@@ -1,6 +1,40 @@
-import { firstFive } from "./oppgaver";
+export {};
 
-const numberArray = [1,2,3,4,5,6,7,8,9,10];
+// OPPGAVE 1
 
-const newArray = firstFive(numberArray);
-console.log(newArray.length);
+// ----- FASIT -----
+
+const returnInput = <T>(input: T) => {
+    return input;
+}
+
+
+
+// OPPGAVE 2
+
+// ----- FASIT -----
+
+const sortById = <T extends {id: number}>(data: Array<T>): Array<T> => {
+    return data.sort((a,b) => a.id - b.id);
+}
+const unsortedData = [{id: 2, value: 'på'}, {id: 1, value: 'hei'}, {id: 3, value: 'deg'}]
+const sortedResult = sortById(unsortedData);
+console.log(sortedResult); // skal nå være sortert etter id
+
+
+// OPPGAVE 3
+
+// ----- FASIT -----
+
+type Menu = {
+    name: string;
+    price: number;
+    discount?: number;
+}
+
+const getMenyValue = <K extends keyof Menu>(menu: Menu, key: K): Menu[K] => {
+    return menu[key];
+}
+
+
+
--- src/6_generics/oppgaver.ts
+++ src/6_generics/oppgaver.ts
@@ -1,33 +1,27 @@
 export {};
 
 // OPPGAVE 1
+
 // Gjør om så funksjonen returnerer samme type som man sendte inn
 const returnInput = (input: any) => {
     return input;
 }
 
-// FASIT:
-// const returnInput = <T>(input: T) => {
-//     return input;
-// }
-
 
 // OPPGAVE 2
+
 // Gjør om slik at vår generiske verdi må inneholde en id som er et tall
 const sortById = <T>(data: Array<T>): Array<T> => {
     return data.sort((a,b) => a.id - b.id);
 }
 
-// FASIT:
-// const sortById = <T extends {id: number}>(data: Array<T>): Array<T> => {
-//     return data.sort((a,b) => a.id - b.id);
-// }
 const unsortedData = [{id: 2, value: 'på'}, {id: 1, value: 'hei'}, {id: 3, value: 'deg'}]
 const sortedResult = sortById(unsortedData);
 console.log(sortedResult); // skal nå være sortert etter id
 
 
 // OPPGAVE 3
+
 type Menu = {
     name: string;
     price: number;
@@ -38,10 +32,7 @@ type Menu = {
 const getMenyValue = (menu: Menu, key: string): any => {
     return menu[key];
 }
-// FASIT:
-// const getMenyValue = <K extends keyof Menu>(menu: Menu, key: K): Menu[K] => {
-//     return menu[key];
-// }
+
 const menu = {name: 'salad', price: 40}
 const menuValue = getMenyValue(menu, 'name');
 console.log(typeof menuValue); // skal gi string hvis key = 'name', number hvis key = 'price' osv
