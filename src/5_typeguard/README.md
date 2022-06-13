827f66aa0f8432857e552f94f265799f4afc953c
Ingeborg Sollid <ingsollid@gmail.com>
2022-06-13 21:18:48 +0200

    Legg til oppgave 5

---
+++ src/5_typeguard/README.md
@@ -0,0 +1,70 @@
+### Type Guards
+
+Type Guards er spesielle operatorer/funksjoner som hjelper til å utlede hvilken type variablen en har innenfor en conditional block. Dette kan også kalles å _narrow down_ typen.
+
+Javascript har operatorene `typeof` og `instanceof` som kan gi informasjon om typen til variabler i runtime. I Typscript kan slike operatorer brukes som type guards.
+
+Vi tar et eksempel:
+
+```typescript
+const formaterPris = (pris: number | string) => {
+  /* 
+    ERROR:
+    Property "toFixed" does not exist on type "string | number"
+    Property "toFixed" does not exist on type "string" 
+  */
+  return pris.toFixed(2);
+};
+```
+Med bruk av operatoren `typeof` kan man utlede hvilken type man før man bruker toFixed().
+
+```typescript
+const formaterPris = (pris: number | string) => {
+  // "price" is of a "number | string" type
+  if (typeof pris === "string") {
+    // "price" is of a "string" type
+    return parseInt(pris, 10).toFixed(2);
+  }
+  // "price" is of a "number" type
+  return pris.toFixed(2);
+};
+```
+
+
+Andre operatorer som også kan brukes som type guards kan du lese mer om her:
+- [📘 instanceOf](https://www.typescriptlang.org/docs/handbook/2/narrowing.html#instanceof-narrowing)
+- [📙 in](https://www.typescriptlang.org/docs/handbook/2/narrowing.html#the-in-operator-narrowing)
+
+📚 Disse kan du lese mer om på 
+
+
+
+### Brukergenererte type guards
+Noen ganger holder det ikke med de innbygde operatorene og da kan brukergenerete type guards være veldig nyttig. En brukergenerert type guards er egentlig kun en funksjon der returtypen er en _type predicate_. 
+I eksempelet under er `isFish` en type guard der returtypen er `pet is Fish` som er en `type predicate`. En _type predicate_ er på formatet `parameterName is Type`.
+
+
+```typescript
+type Fish ={
+    name: string,
+    swim: () => void
+}
+
+type Bird = {
+    name: string,
+    fly: () => void
+}
+
+type Pet = Fish | Bird;
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+if (isFish(pet)) {
+  pet.swim();
+} else {
+  pet.fly();
+}
+```
+
---
+++ src/5_typeguard/oppgaver.ts
@@ -0,0 +1,60 @@
+
+// OPPGAVE: Lag brukerdefinerte typeguards for de tre ulike lunsjtypene. Slik at getLunsj kan output riktig informasjon
+
+
+
+
+// I denne oppgaven trenger  vi litt lunsjtyper. De ulike lunsjrett-typene har noen variabler til felles, 
+// men også noen unike variabler til sin type.
+type VerdensKjøkkenType = 'ASIATISK' | 'ITALIENSK' | 'FRANSK' | 'INDISK' | 'MEKSIKANSK' | 'NORSK';
+type KjøttType = 'KYLLING' | 'SVIN' | 'STORFE' | 'KJØTTDEIG' | 'PØLSE';
+enum FiskArtType { 'LAKS' = 'laks', 'ØRRET' = 'ørret', 'HYSE' = 'hyse', 'TORSK' = 'torsk', 'SEI' = 'sei' }
+
+type GenerellLunsjInformasjon = {
+    allergener: string[],
+    navn: string,
+    antallGangerServert: number,
+    verdensKjøkken: VerdensKjøkkenType,
+}
+
+type KjøttholdingRett = GenerellLunsjInformasjon & {
+    kjøtt: KjøttType,
+}
+
+type FiskeRett = GenerellLunsjInformasjon & {
+    fiskeArt: FiskArtType,
+    isSuppe: boolean,
+}
+
+type VegetarRett = GenerellLunsjInformasjon & {
+    isMedBønner: boolean;
+    antallGrønnsaker: number
+}
+
+type LunsjRett = KjøttholdingRett | VegetarRett | FiskeRett;
+
+const lunsjRetter: LunsjRett[] = [
+    { navn: "Chilli con torsk", allergener: ["fisk"], antallGangerServert: 3, verdensKjøkken: 'MEKSIKANSK', isSuppe: false, fiskeArt: FiskArtType.TORSK },
+    { navn: "Soppgrateng", allergener: ["gluten"], antallGangerServert: 2, verdensKjøkken: 'FRANSK', isMedBønner: false, antallGrønnsaker: 1 },
+    { navn: "Bergens fiskesuppe", allergener: ["fisk", "skalldyr", "gluten"], antallGangerServert: 5, verdensKjøkken: 'NORSK', isSuppe: true, fiskeArt: FiskArtType.SEI },
+    { navn: "Fiskekaker med ovnsbakte grønnsaker", allergener: ["fisk", 'nøtter'], antallGangerServert: 10, verdensKjøkken: 'NORSK', isSuppe: false, fiskeArt: FiskArtType.HYSE },
+    { navn: "Tikka Masala", allergener: ["laktose", 'nøtter'], antallGangerServert: 10, verdensKjøkken: 'INDISK', kjøtt: 'KYLLING' },
+    { navn: "Loaded veggie nachos", allergener: [], antallGangerServert: 2, verdensKjøkken: 'MEKSIKANSK', isMedBønner: true, antallGrønnsaker: 6 }
+]
+
+
+
+
+export const getLunsj = (lunsjRetter: LunsjRett[]): void => {
+    lunsjRetter.forEach((lunsjRett) => {
+        //TODO: Dersom det er en vegetarrett output hvor mange grønnsaker den har
+        //TODO: Dersom det er en fiskerett output hvilken fiskeart som blir brukt
+        //TODO: Dersom det er kjøtt output kjøtttype.
+    })
+};
+
+
+getLunsj(lunsjRetter);
+
+
+
