3cc8508a2d63079228cb6d9c4b18c17b9e9bf0a3
Håkon Jarvis Drage <hakon.jarvis.drage@gmail.com>
2022-06-21 19:36:10 +0200

    Update oppgaver.ts

--- typescript-oppgaver/3_union_intersection_types/oppgaver.ts
+++ typescript-oppgaver/3_union_intersection_types/oppgaver.ts
@@ -1,4 +1,16 @@
 // Oppgave 1:
+// Definer tre typer 'forrett', 'hovedrett' og 'dessert'. Hver type skal inneholde følgende:
+// 'forrett': drikke, matType (to valg - enten 'vegetar' eller 'kjøtt')
+// 'hovedrett': drikke, matType og tilbehør
+// 'dessert': drikke og en ikke-obligatorisk egenskap på om desserten skal være vegansk.
+// Definér en type som heter 'Middag' og som er en intersection type av 'forrett', 'hovedrett' og 'dessert'.
+// (EKSTRA) Hva er konsekvensene av å bruke et objekt av typen 'Middag' til å representere en hel middag?
+
+// Oppgave 2:
+// Definer en type, 'UtvidetMiddag', som utvider ´Middag´ slik at den kan holde på egenskapene
+// 'spritType' (som kan enten være 'vodka', 'snaps' eller 'portvin') og antall enheter
+
+// Oppgave 3:
 // Skriv om funksjonen slik at inputparametrene a og b er en union type av string og number.
 // Hva må du erstatte '??' med for at funksjonen skal returnere? (Tips --> sjekk ut hvordan 
 // du kan bruke 'typeof' til din fordel) 
@@ -13,14 +25,3 @@ function add(a: any, b: any) {
   throw new Error("Parameters must be numbers or strings");
 }
 
-// Oppgave 2:
-// Definer tre typer 'forrett', 'hovedrett' og 'dessert'. Hver type skal inneholde følgende:
-// 'forrett': drikke, matType (to valg - enten 'vegetar' eller 'kjøtt')
-// 'hovedrett': drikke, matType og tilbehør
-// 'dessert': drikke og en ikke-obligatorisk egenskap på om desserten skal være vegansk.
-// Definér en type som heter 'Middag' og som er en intersection type av 'forrett', 'hovedrett' og 'dessert'.
-// (EKSTRA) Hva er konsekvensene av å bruke et objekt av typen 'Middag' til å representere en hel middag?
-
-// Oppgave 3:
-// Definer en type, 'UtvidetMiddag', som utvider ´Middag´ slik at den kan holde på egenskapene
-// 'spritType' (som kan enten være 'vodka', 'snaps' eller 'portvin') og antall enheter
