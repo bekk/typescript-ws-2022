b259e45c10f8425bad7a87f2d275da836c79ff1a
Håkon Jarvis Drage <hakon.jarvis.drage@gmail.com>
2022-06-23 13:19:35 +0200

    Update oppgaver.ts

--- typescript-oppgaver/2_enums/oppgaver.ts
+++ typescript-oppgaver/2_enums/oppgaver.ts
@@ -1,8 +1,8 @@
-// Oppgave 1: Lag en enum som definerer et måltid med egenskapene 'verdenskjøkken'
+// Oppgave 1: Lag en type som definerer et måltid med egenskapene 'verdenskjøkken'
 // (f.eks. italiensk, thai, kinesisk) og 'kjøttType' (f.eks. svin, okse, lam eller kylling))
 
 // Oppgave 2
-// Hvilke to muligheter har vi på å sette c.kind til, for at feilen skal forsvinne?
+// Hva må vi sette c.kind til, for at feilen skal forsvinne?
 enum ShapeKind {
   Circle,
   Square,
@@ -22,6 +22,9 @@ let c: Circle = {
   kind: ShapeKind.Square, // Hva kan vi sette kind til?
   radius: 100,
 };
+
+// Oppgave 2 - Ekstra
+// Har du testet å sette kind til 0 eller 1? Hva med 1337? Kompilerer koden?
 
 // Oppgave 3 (diskusjon med personen ved siden av deg) hvilke pros og cons finnes det for å bruke enums?
 
